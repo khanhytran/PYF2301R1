5742c66525c78f97d0c8b7e730dc1d89b64d4208
khanhytran <123288181+khanhytran@users.noreply.github.com>
2023-02-04 08:46:37 +0700

    upload file python for learncodegym.com

---
+++ displayInformation.py
@@ -0,0 +1,9 @@
+#thông tin cá nhân
+
+ho_ten = "Trần Khánh Ý"
+ngay_sinh = "20/04/1999"
+dia_chi = "tp.HCM"
+muc_dich = "học cách sử dụng ngôn ngữ python áp dụng cho công việc."
+
+
+print("Tôi tên là " + ho_ten + ", " + "sinh ngày " + ngay_sinh +". "+"Hiện tại đang làm việc và sinh sống ở " + dia_chi + ". " + "Tôi đến với khóa học python để " + muc_dich)
